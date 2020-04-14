62f1d9baa49fdbf22334d59cdfc8dd23d5204683
christian ramos <christian-ruben1@hotmail.com>
2020-04-13 22:35:04 -0500

    Componente Carrera

--- src/App.vue
+++ src/App.vue
@@ -7,35 +7,32 @@
         color ="secondary"
         app
       >
-        <v-list dense>
+        <v-list flat dense shaped>
         <v-list-group
-          prepend-icon="home"
+          prepend-icon="import_contacts"
           no-action
-          
+          color="indigo"
         >
           <template v-slot:activator>
             <v-list-item-content>
-              <v-list-item-title>Home</v-list-item-title>
+              <v-list-item-title>Registros</v-list-item-title>
+              <v-list-item-title>Académicos</v-list-item-title>
             </v-list-item-content>
           </template>
   
-          <v-list-item
-            :to="{ name: 'inicio' }"
-          >
+          <v-list-item :to="{ name: 'carrera' }">
               <v-list-item-action>
-                <v-icon>insert_chart</v-icon>
+                <v-icon>assignment</v-icon>
               </v-list-item-action>
               <v-list-item-content>
                 <v-list-item-title>
-                  Gráficos 
-                </v-list-item-title><v-list-item-title>
-                 Estadísticos
+                  Carreras 
                 </v-list-item-title>
-              </v-list-item-content>
+                </v-list-item-content>
           </v-list-item>
 
             <v-list-item
-            :to="{ name: 'inicio' }"
+            :to="{ name: 'carrera' }"
           >
               <v-list-item-action>
                 <v-icon>insert_chart</v-icon>
@@ -62,9 +59,13 @@
         <v-toolbar-title>System Uni</v-toolbar-title>
       </v-app-bar>
 
-      <v-content>
-        <router-view></router-view>
-      </v-content>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-slide-y-transition mode="out-in">
+          <router-view/>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
       <v-footer  color="primary2" light app>
         <span class="white--text">&copy; 2020</span>
       </v-footer>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Carrera from '../components/Carrera.vue'
 Vue.use(VueRouter)
 
   const routes = [
-
+    {
+      path: '/carrera', //este es la ruta del navegador
+      name: 'carrera', //este nombre es el que se usa en el html
+      component: Carrera,
+    },
     
 ]
 
